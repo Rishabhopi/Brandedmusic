2c4fe73ce30832739e7fd0c5e5bf4c85bf1b7ce4
TEAM ISTKHAR <mdistkhar1078@gmail.com>
2024-08-09 06:49:08 +0530

    Update play.py

--- BrandrdXMusic/utils/inline/play.py
+++ BrandrdXMusic/utils/inline/play.py
@@ -33,25 +33,26 @@ def stream_markup_timer(_, vidid, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "❥—————————"
+        
+       bar = "👻▰▱▱▱▱▱▱▱▰"
     elif 10 < umm < 20:
-        bar = "—❥————————"
+        bar = "▰Ⓘ︎▱▱▱▱▱▱▱▰"
     elif 20 <= umm < 30:
-        bar = "——❥———————"
+        bar = "▰▱Ⓢ︎▱▱▱▱▱▱▰"
     elif 30 <= umm < 40:
-        bar = "———❥——————"
+        bar = "▰▱▱Ⓣ︎▱▱▱▱▱▰"
     elif 40 <= umm < 50:
-        bar = "————❥—————"
+        bar = "▰▱▱▱Ⓚ︎▱▱▱▱▰"
     elif 50 <= umm < 60:
-        bar = "—————❥————"
+        bar = "▰▱▱▱▱Ⓗ︎▱▱▱▰"
     elif 60 <= umm < 70:
-        bar = "——————❥———"
+        bar = "▰▱▱▱▱▱Ⓐ︎▱▱▰"
     elif 70 <= umm < 80:
-        bar = "———————❥——"
+        bar = "▰▱▱▱▱▱▱Ⓡ︎▱▰"
     elif 80 <= umm < 95:
-        bar = "————————❥—"
+        bar = "▰Ⓘ︎Ⓢ︎Ⓣ︎Ⓚ︎Ⓗ︎Ⓐ︎Ⓡ︎▰"
     else:
-        bar = "—————————❥"
+        bar = "▰▱Ⓟ︎Ⓤ︎Ⓡ︎Ⓥ︎Ⓘ︎▱▰"
     buttons = [
          [
             InlineKeyboardButton(
@@ -68,10 +69,10 @@ def stream_markup_timer(_, vidid, chat_id, played, dur):
         ],
         [
             InlineKeyboardButton(
-                text="⛦ ᴏᴡɴᴇʀ ❥", url=f"https://t.me/Aniket_1243"
+                text="⛦ ᴏᴡɴᴇʀ ❥", url=f"https://t.me/lll_ISTKHAR_ll"
             ),
             InlineKeyboardButton(
-                text="⛦ ᴄʜᴀᴛ ❥", url=f"https://t.me/AniketAbout"
+                text="⛦ ᴄʜᴀᴛ ❥", url=f"https://t.me/+4cgcV3Q_2CI2YjNl"
             ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -90,10 +91,10 @@ def stream_markup(_, videoid, chat_id):
         ],
         [
             InlineKeyboardButton(
-                text="⛦ ᴏᴡɴᴇʀ ❥", url=f"https://t.me/Aniket_1243"
+                text="⛦ ᴏᴡɴᴇʀ ❥", url=f"https://t.me/lll_ISTKHAR_ll"
             ),
             InlineKeyboardButton(
-                text="⛦ ᴄʜᴀᴛ ❥", url=f"https://t.me/AniketAbout"
+                text="⛦ ᴄʜᴀᴛ ❥", url=f"https://t.me/+4cgcV3Q_2CI2YjNl"
             ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
