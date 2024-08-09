a02fd9ce1fac0e4f9c5fed1cca665483023dea9d
TEAM ISTKHAR <mdistkhar1078@gmail.com>
2024-08-09 06:39:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,18 +36,18 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Danishzain05/ZainMusic",
+    "https://github.com/istkharalam62/PurviMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/AniketAbout")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/AniketAbout")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/purvi_support")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/PURVI_UPDATES")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
-AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
+AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", True))
 
 # Auto Gcast/Broadcast Handler (True = broadcast on , False = broadcast off During Hosting, Dont Do anything here.)
 AUTO_GCAST = os.getenv("AUTO_GCAST")
